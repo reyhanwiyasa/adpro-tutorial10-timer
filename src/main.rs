3cd4696e0dded1ca96ae9b61b41053ec73691a63
reyhanwiyasa <reyhanwiyasa@gmail.com>
2024-05-07 14:56:49 +0700

    finish readme

--- src/main.rs
+++ src/main.rs
@@ -24,11 +24,23 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("ReyhanWiyasa's computer: done!");
     });
+    spawner.spawn(async {
+        println!("ReyhanWiyasa's computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("ReyhanWiyasa's computer: done2!");
+    });
+    spawner.spawn(async {
+        println!("ReyhanWiyasa's computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("ReyhanWiyasa's computer: done3!");
+    });
 
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    print!("ReyhanWiyasa's computer: hey hey");
-    drop(spawner);
+    print!("ReyhanWiyasa's computer: hey hey\n");
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
